4c90a42145c06f4f0dd1be0da618e6fda94c1d7e
Pranto Bapary <pranto.bapary01@gmail.com>
2025-09-10 23:40:20 +0600

    fix hamburger menu for smaller devices

--- Day 7/assets/js/script.js
+++ Day 7/assets/js/script.js
@@ -6,6 +6,14 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
 
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+    hamburger.classList.remove("active");
+  });
+});
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
@@ -15,70 +23,69 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  
 });
 
 // Product Carousel
-$('.product-carousel .owl-carousel').owlCarousel({
-    loop:true,
-    autoplay:true,
-    margin:25,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        768:{
-            items:1,
-            nav:false
-        },
-        991:{
-            items:4,
-            nav:false,
-            loop:false
-        }
-    }
+$(".product-carousel .owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  margin: 25,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    768: {
+      items: 1,
+      nav: false,
+    },
+    991: {
+      items: 4,
+      nav: false,
+      loop: false,
+    },
+  },
 });
 
 // Blog Carousel
-$('.blog-carousel .owl-carousel').owlCarousel({
-    loop:true,
-    autoplay:true,
-    margin:30,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-        },
-        768:{
-            items:3,
-        },
-        991:{
-            items:2,
-            loop:true,
-        }
-    }
+$(".blog-carousel .owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  margin: 30,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    768: {
+      items: 3,
+    },
+    991: {
+      items: 2,
+      loop: true,
+    },
+  },
 });
 
 // Blog Carousel
-$('.brand-carousel .owl-carousel').owlCarousel({
-    loop:true,
-    autoplay:true,
-    margin:80,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:4,
-        },
-        768:{
-            items:4,
-        },
-        991:{
-            items:4,
-            loop:true,
-        }
-    }
+$(".brand-carousel .owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  margin: 80,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 4,
+    },
+    768: {
+      items: 4,
+    },
+    991: {
+      items: 4,
+      loop: true,
+    },
+  },
 });
 
 // AOS

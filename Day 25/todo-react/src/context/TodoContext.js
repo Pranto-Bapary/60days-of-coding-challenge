237d15e4552403f39e0b00eb9115893813411e56
Pranto-Bapary <pranto.bapary01@gmail.com>
2025-12-20 23:52:00 +0600

    Completed Day 25 - ToDo using Context API + Local Storage

---
+++ Day 25/todo-react/src/context/TodoContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      title: "Task 1",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const TodoProvider = TodoContext.Provider;
+
+export default function useTodo() {
+  return useContext(TodoContext);
+}
